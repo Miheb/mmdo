800c6764ad544d055cada3d06ad4726f0b63fe8f
mastoura <iheb_mastoura1996@hotmail.com>
2018-03-14 16:11:45 +0100

    vd

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+export interface result {
+     
+  title:string ; 
+  author:string; 
+  date:string;
+
+}
+
+const fake: result[] = [
+{title : 'iheb' , author : 'the best' , date: '14/09/1996' },
+{title : 'mastoura' , author : 'the last' , date: '14/09/2096' }
+]
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+   result : result[] ;
 
   constructor(public navCtrl: NavController) {
-
+   this.result=fake ;
   }
 
 }
